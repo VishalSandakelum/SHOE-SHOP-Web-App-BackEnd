2d21638274116c305d991c8274b6b2ed58885b7b
vishalgit2005 <vishalsandakelum20@gmail.com>
2024-04-29 11:32:12 +0530

    basic architecture created.

---
+++ src/main/java/lk/ijse/finalcoursework/shoeshop/persistence/entity/Customer.java
@@ -0,0 +1,9 @@
+package lk.ijse.finalcoursework.shoeshop.persistence.entity;
+
+/**
+ * @author: Vishal Sandakelum,
+ * @Runtime version: 11.0.11+9-b1341.60 amd64
+ **/
+
+public class Customer {
+}
---
+++ src/main/java/lk/ijse/finalcoursework/shoeshop/persistence/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package lk.ijse.finalcoursework.shoeshop.persistence.repository;
+
+/**
+ * @author: Vishal Sandakelum,
+ * @Runtime version: 11.0.11+9-b1341.60 amd64
+ **/
+
+public class CustomerRepository {
+}
