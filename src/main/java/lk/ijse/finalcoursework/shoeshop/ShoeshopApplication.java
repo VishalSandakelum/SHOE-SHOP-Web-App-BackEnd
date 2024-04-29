df339f273c9082a7e58d8f0249444016abbec57a
vishalgit2005 <vishalsandakelum20@gmail.com>
2024-04-29 10:57:26 +0530

    initial commit

---
+++ src/main/java/lk/ijse/finalcoursework/shoeshop/ShoeshopApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.finalcoursework.shoeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/finalcoursework/shoeshop/ShoeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.finalcoursework.shoeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
