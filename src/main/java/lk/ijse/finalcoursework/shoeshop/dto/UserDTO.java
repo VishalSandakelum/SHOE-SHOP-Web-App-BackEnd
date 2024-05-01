656ae1e53321d6b3ae00392edb6924696f534f87
vishalgit2005 <vishalsandakelum20@gmail.com>
2024-05-01 06:54:51 +0530

    create user dto & role enum

---
+++ src/main/java/lk/ijse/finalcoursework/shoeshop/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package lk.ijse.finalcoursework.shoeshop.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lk.ijse.finalcoursework.shoeshop.util.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: Vishal Sandakelum,
+ * @Runtime version: 11.0.11+9-b1341.60 amd64
+ **/
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    @NotBlank(message = "Email Cannot Be Null")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$\n", message = "Email not valid")
+    private String email;
+    @NotBlank(message = "Password Cannot Be Null")
+    private String password;
+    @NotBlank(message = "Role Cannot Be Null")
+    @Pattern(regexp = "^(?:USER|ADMIN)$\n", message = "Role not valid")
+    private Role role;
+}
---
+++ src/main/java/lk/ijse/finalcoursework/shoeshop/util/Role.java
@@ -0,0 +1,6 @@
+package lk.ijse.finalcoursework.shoeshop.util;
+
+public enum Role {
+    USER,
+    ADMIN
+}
